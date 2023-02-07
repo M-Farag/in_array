8a381b53d5c499d46d0e5984284a471c172d1e1d
M-Farag <mina.farag@icloud.com>
2023-02-07 23:00:27 +0200

    UPD Allow the user to write a word to search it in the list

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::io;
+
 fn main() {
     let items:[&str;5] = ["text_1","text_2","text_3","text_4","text_5"];
-    let item:&str = "text_1";
+    let mut item:String = String::new();
+
+    println!("Please write a word to search it in the list of items: {:#?}",items);
+    io::stdin().read_line(&mut item).expect("Err reading your word");
+    
+    let item = item.trim();
     
     match in_array(item, &items) {
         true => println!("Item exist in the array"),
         false => println!("Item does not exist in the array")
     }
-    
+
 }
 
 
