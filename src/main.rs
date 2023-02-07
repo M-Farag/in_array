9338d0b9315d6450a6d5d0fa65f78d84c7384c0f
M-Farag <mina.farag@icloud.com>
2023-02-07 22:55:52 +0200

    UPD Basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let items:[&str;5] = ["text_1","text_2","text_3","text_4","text_5"];
+    let item:&str = "text_1";
+    
+    match in_array(item, &items) {
+        true => println!("Item exist in the array"),
+        false => println!("Item does not exist in the array")
+    }
+    
+}
+
+
+fn in_array(needle:&str,haystack:&[&str;5]) -> bool
+{
+    for &item in haystack {
+        if needle == item {
+            return true
+        }
+    }
+    false
 }
